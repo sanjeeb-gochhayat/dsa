6bc02178c2b9ea28e77a4a2f56cdd2d1b3575b3e
INC02037 <sanjeeb.g@incture.com>
2025-06-02 13:03:34 +0530

    ..

--- Basic/One.js
+++ Basic/One.js
@@ -1,1 +1,14 @@
-console.log("hello");
+//Write an function that searches for an element in an array and returns the index, if the element is not present then just return -1
+
+let searchElemet = (arr, el) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === el) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+let arr = [4, 2, 0, 10, 8, 30];
+let res = searchElemet(arr, 10);
+console.log(res);
